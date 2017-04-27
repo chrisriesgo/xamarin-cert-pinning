6184702c2fbd642ff40b129dd3f8acd79231e8e4
chrisriesgo <cmriesgo@gmail.com>
2017-04-27 12:20:24 -0500

    Added in app files

---
+++ PinCerts/PinCerts/PinCertsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace PinCerts
+{
+	public partial class PinCertsPage : ContentPage
+	{
+		PinCertsViewModel ViewModel => BindingContext as PinCertsViewModel;
+		
+		public PinCertsPage()
+		{
+			InitializeComponent();
+		}
+		
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			FetchData();
+		}
+
+		void LoadData(object sender, System.EventArgs e)
+		{
+			FetchData();
+		}
+		
+		async void FetchData()
+		{
+			await ViewModel.FetchData().ConfigureAwait(false);
+		}
+	}
+}
